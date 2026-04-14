2711c2dd59e86b91d2d33b8959553a8291491331
samuelvianac <samuelvianac3@gmail.com>
2026-04-14 15:43:53 -0300

    Adiciona ClienteService

---
+++ src/main/java/com/samuek/apibanco/service/ClienteService.java
@@ -0,0 +1,30 @@
+package com.samuek.apibanco.service;
+
+import com.samuek.apibanco.domain.Cliente;
+import com.samuek.apibanco.repository.ClienteRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService {
+
+    private final ClienteRepository clienteRepository;
+
+    public ClienteService(ClienteRepository clienteRepository) {
+        this.clienteRepository = clienteRepository;
+    }
+
+    public List<Cliente> listarTodos() {
+        return clienteRepository.findAll();
+    }
+
+    public Optional<Cliente> buscarPorId(Long id) {
+        return clienteRepository.findById(id);
+    }
+
+    public Cliente salvar(Cliente cliente) {
+        return clienteRepository.save(cliente);
+    }
+}
