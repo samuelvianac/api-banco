08c282ad7b65b82e2e7abb78b9c4f97757199d51
samuelvianac <samuelvianac3@gmail.com>
2026-04-14 15:41:05 -0300

    Adiciona entidade Cliente

---
+++ src/main/java/com/samuek/apibanco/domain/Cliente.java
@@ -0,0 +1,52 @@
+package com.samuek.apibanco.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    public Cliente() {
+    }
+
+    public Cliente(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
