24772f8219442997b4b03f3c24a4448b4ea32ab1
samuelvianac <samuelvianac3@gmail.com>
2026-04-14 15:38:53 -0300

    Adiciona classe principal da aplicação

---
+++ src/main/java/com/samuek/apibanco/ApibancoApplication.java
@@ -0,0 +1,12 @@
+package com.samuek.apibanco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApibancoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApibancoApplication.class, args);
+    }
+}
